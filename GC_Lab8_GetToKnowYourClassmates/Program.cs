4b61632ec69b6025715d9fdeb4b2ffb3ef9f636c
Tomás <tomasrcalvo@gmail.com>
2020-04-30 09:00:31 -0400

    Refactored validation.

--- GC_Lab8_GetToKnowYourClassmates/Program.cs
+++ GC_Lab8_GetToKnowYourClassmates/Program.cs
@@ -6,15 +6,22 @@ namespace GC_Lab8_GetToKnowYourClassmates
     {
         static void Main(string[] args)
         {
-            string[,] studentDossiers = { { "Kim", "Driscoll", "Detroit, MI", "Falafel" }, { "Kim", "Wexler", "Albuqueruqe, NM", "Takeout"}, { "Saul", "Goodman", "Albuquerque, NM", "Lobster" }, { "Tomás", "Calvo", "Bay City, MI", "Cheddar Bay Biscuits" }, { "Cary", "Grant", "Hollywood, CA", "Chicken Almondine" }, { "Tyler", "Van Eck", "Wyoming, MI", "Shrimp Scampi" }, { "Andy", "Balash", "Auburn, MI", "Turkey Drumsticks" }, { "Michael", "Jarema", "Bay City, MI", "Tater Tots" }, { "Evan", "Doorshorst", "Kansas City, MO", "Fish Tacos" }, { "Alvaro", "Romero-Gibu", "Lima, Peru", "Avocado Toast" }, { "Colby", "Roanhorse", "Rochester, MI", "Coffee" }, { "Sofía", "Ramirez-Hernandez", "Rochester, MI", "Hamburgers" }, { "Kimmy", "Snyder", "Zeeland, MI", "Risotto" }, { "Mychal", "Hilken", "Jackson, MI", "Ham" }, { "Paul", "Clarke", "St. Augustine, FL", "Lap Song Su Chi" }, { "Kelly", "Meyer", "Luddington, MI", "Fish and Chips" }, { "Laurie", "Reeves", "Grand Rapids, MI", "Saffron Steak" }, { "Diego", "Calvo", "Bay City, MI", "Bone Broth" }, { "Salma", "Hayek", "Mexico City, MX", "Bone Broth" }, { "Laura", "Dern", "Santa Clara, CA", "Anything Jell-O" } };
+            //DATA SET AS A TWO-DIMENSIONAL ARRAY
+            string[,] studentDossiers = { { "Kim", "Driscoll", "Detroit, MI", "Falafel" },
+                { "Kim", "Wexler", "Albuqueruqe, NM", "Takeout" },
+                { "Saul", "Goodman", "Albuquerque, NM", "Lobster" },
+                { "Tomás", "Calvo", "Bay City, MI", "Cheddar Bay Biscuits" }, { "Cary", "Grant", "Hollywood, CA", "Chicken Almondine" }, { "Tyler", "Van Eck", "Wyoming, MI", "Shrimp Scampi" }, { "Andy", "Balash", "Auburn, MI", "Turkey Drumsticks" }, { "Michael", "Jarema", "Bay City, MI", "Tater Tots" }, { "Evan", "Doorshorst", "Kansas City, MO", "Fish Tacos" }, { "Alvaro", "Romero-Gibu", "Lima, Peru", "Avocado Toast" }, { "Colby", "Roanhorse", "Rochester, MI", "Coffee" }, { "Sofía", "Ramirez-Hernandez", "Rochester, MI", "Hamburgers" }, { "Kimmy", "Snyder", "Zeeland, MI", "Risotto" }, { "Mychal", "Hilken", "Jackson, MI", "Ham" }, { "Paul", "Clarke", "St. Augustine, FL", "Lap Song Su Chi" }, { "Kelly", "Meyer", "Luddington, MI", "Fish and Chips" }, { "Laurie", "Reeves", "Grand Rapids, MI", "Saffron Steak" }, { "Diego", "Calvo", "Bay City, MI", "Bone Broth" }, { "Salma", "Hayek", "Mexico City, MX", "Bone Broth" }, { "Laura", "Dern", "Santa Clara, CA", "Anything Jell-O" } };
+
+            //DATA SET AS A DICTIONARY
+            //studentDossiers<>
 
             Console.Write("Welcome to our C# class.");
             bool loop = true;
             while (loop)
             {
-                int index = ValidateStudentNumber(studentDossiers);
-                ValidateInfoSelection(index, studentDossiers);
-                ValidateLoop("another student");
+                int studentNumber = ValidateStudentNumber(studentDossiers);
+                ValidateInfoSelection(studentNumber -1, studentDossiers);
+                loop = ValidateLoop("another student");
             }
 
             Console.WriteLine("Thanks!");
@@ -22,34 +29,38 @@ namespace GC_Lab8_GetToKnowYourClassmates
 
         public static int ValidateStudentNumber(string[,] array)
         {
-            int response = 0;
+            int studentNumber = -1;
             bool validation = false;
             while (!validation)
             {
-                try
-                {
+                
                     Console.WriteLine("Which student would you like to learn more about? Enter a number 1-20.");
-                    response = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Student {response} is {array[response - 1, 0]} {array[response -1, 1]}.");
-                }
-                catch (FormatException e1)
-                {
-                    Console.WriteLine(e1.Message);
-                }
-                catch (ArithmeticException e2)
-                {
-                    Console.WriteLine(e2.Message);
-                }
-                catch (IndexOutOfRangeException e3)
-                {
-                    Console.WriteLine(e3.Message);
-                }
-                finally
-                {
-                    validation = true;
-                }
+                    bool isInt = int.TryParse(Console.ReadLine(), out studentNumber);
+                    if (isInt)
+                    {
+                        try
+                        {
+                        Console.WriteLine($"Student {studentNumber} is {array[studentNumber - 1, 0]} {array[studentNumber -1, 1]}.");
+                        validation = true;
+                        }
+                        catch (FormatException e1)
+                        {
+                            Console.WriteLine(e1.Message);
+                        }
+                        catch (ArithmeticException e2)
+                        {
+                            Console.WriteLine(e2.Message);
+                        }
+                        catch (IndexOutOfRangeException e3)
+                        {
+                            Console.WriteLine(e3.Message);
+                        }
+                    } else
+                    {
+                        throw new Exception("Student number must be an integer 1-20.");
+                    }
             }
-            return response - 1;
+            return studentNumber;
         }
 
         public static void ValidateInfoSelection(int index, string[,] array)
@@ -93,32 +104,29 @@ namespace GC_Lab8_GetToKnowYourClassmates
 
         public static bool ValidateLoop(string studentName)
         {
-            bool valid = false;
-            while (!valid)
+            Console.WriteLine($"Would you like to know more about {studentName}? Enter \"yes\" or \"no\".");
+            try
             {
-                try
+                string response = Console.ReadLine().ToLower().Trim();
+                if (response == "yes")
                 {
-                    Console.WriteLine($"Would you like to know more about {studentName}? Enter \"yes\" or \"no\".");
-                    string response = Console.ReadLine().ToLower().Trim();
-                    if (response == "yes")
-                    {
-                        return true;
-                    }
-                    else if (response == "no")
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid entry.");
-                    }
+                    return true;
                 }
-                catch (FormatException e1)
+                else if (response == "no")
                 {
-                    Console.WriteLine(e1.Message);
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry.");
+                    return false;
                 }
             }
-            return true;
+            catch (FormatException e1)
+            {
+                Console.WriteLine(e1.Message);
+                return false;
+            }
         }
     }
 }
